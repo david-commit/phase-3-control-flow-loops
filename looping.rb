f7ebb9f6bd1f58ec0119c549198861669fd4aa2f
david-commit <david.ondiege@students.moringaschool.com>
2022-10-17 12:23:10 +0300

    Incomplete Reverse String Tests

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
+require "pry"
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +24,28 @@ end
 
 def fizzbuzz_printer
   # your code here
+  num = 1
+  while
+    num < 101
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end  
+    num += 1
+  end
 end
+# binding.pry
 
 def reverse_string(str)
   # your code here
+  i = 0
+  while i <= str.length
+    puts str.reverse
+    i += 1
+  end
 end
